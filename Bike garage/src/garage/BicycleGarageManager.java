de37d95ac1f5f93f3dc9ad7afc743273fc4ff4bc
Fredrik Ekstedt Karpers <Fredrik@karpers.se>
2015-05-14 13:19:46 +0200

    Tog bort testkommentaren

--- Bike garage/src/garage/BicycleGarageManager.java
+++ Bike garage/src/garage/BicycleGarageManager.java
@@ -24,7 +24,7 @@ import interfaces.PinCodeTerminal;
 public class BicycleGarageManager {// TODO implement methods specified by course
 									// homepage:
 									// http://cs.lth.se/etsa01/projekt-2015/specifikation-av-mjukvaran/
-//hej fredrik testar
+
 	private BarcodePrinter printer;
 	private BarcodeReader reader;
 	private ElectronicLock entryLock, exitLock;
